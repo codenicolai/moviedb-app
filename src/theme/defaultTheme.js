1d4bb93f9259e7025c41bd4d62b7ee0c831ae6ab
Leonardo Nicolai <nicolai.padilha@codengage.com>
2020-10-20 01:23:56 -0300

    add theme file

---
+++ src/theme/defaultTheme.js
@@ -0,0 +1,117 @@
+const colors = {
+  // grey scale
+  white: "#ffffff",
+  gray: "#808080 ",
+  lightGray: "#979797",
+  black: "#000000",
+
+  // topic
+  red: "#FF001F",
+  green: "#7CBC37",
+  yellow: "#F6A623",
+  blue: "#4990E2",
+  purple: "#BD10E0",
+};
+
+const breakpoints = ["414px", "576px", "768px", "1024px", "1366px", "1920px"];
+
+breakpoints.base = breakpoints[0];
+breakpoints.mobile = breakpoints[1];
+breakpoints.tablet = breakpoints[2];
+breakpoints.web = breakpoints[3];
+breakpoints.hd = breakpoints[4];
+breakpoints.fullhd = breakpoints[5];
+
+const fontSizes = [10, 12, 14, 16, 18, 24, 32, 44];
+fontSizes.xsmall = fontSizes[0];
+fontSizes.small = fontSizes[1];
+fontSizes.medium = fontSizes[2];
+fontSizes.default = fontSizes[3];
+fontSizes.large = fontSizes[4];
+fontSizes.xlarge = fontSizes[5];
+
+const fontWeights = [300, 500, 700];
+fontWeights.regular = fontWeights[0];
+fontWeights.medium = fontWeights[1];
+fontWeights.bold = fontWeights[2];
+
+const space = [
+  0,
+  2,
+  4,
+  6,
+  8,
+  10,
+  12,
+  14,
+  16,
+  18,
+  20,
+  22,
+  24,
+  26,
+  28,
+  30,
+  32,
+  40,
+  54,
+  64,
+  128,
+  144,
+  256,
+];
+
+const shadows = [
+  "none",
+  "0px 2px 4px rgba(0, 0, 0, 0.1)",
+  "0px 4px 8px rgba(0, 0, 0, 0.1)",
+  "0px 8px 16px rgba(0, 0, 0, 0.1)",
+  "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
+  "0px 3px 5px -1px rgba(0,0,0,0.2),0px 5px 8px 0px rgba(0,0,0,0.14),0px 1px 14px 0px rgba(0,0,0,0.12)",
+  "0px 3px 5px -1px rgba(0,0,0,0.2),0px 6px 10px 0px rgba(0,0,0,0.14),0px 1px 18px 0px rgba(0,0,0,0.12)",
+  "0px 4px 5px -2px rgba(0,0,0,0.2),0px 7px 10px 1px rgba(0,0,0,0.14),0px 2px 16px 1px rgba(0,0,0,0.12)",
+  "0px 5px 5px -3px rgba(0,0,0,0.2),0px 8px 10px 1px rgba(0,0,0,0.14),0px 3px 14px 2px rgba(0,0,0,0.12)",
+  "0px 5px 6px -3px rgba(0,0,0,0.2),0px 9px 12px 1px rgba(0,0,0,0.14),0px 3px 16px 2px rgba(0,0,0,0.12)",
+  "0px 6px 6px -3px rgba(0,0,0,0.2),0px 10px 14px 1px rgba(0,0,0,0.14),0px 4px 18px 3px rgba(0,0,0,0.12)",
+  "0px 6px 7px -4px rgba(0,0,0,0.2),0px 11px 15px 1px rgba(0,0,0,0.14),0px 4px 20px 3px rgba(0,0,0,0.12)",
+  "0px 7px 8px -4px rgba(0,0,0,0.2),0px 12px 17px 2px rgba(0,0,0,0.14),0px 5px 22px 4px rgba(0,0,0,0.12)",
+  "0px 7px 8px -4px rgba(0,0,0,0.2),0px 13px 19px 2px rgba(0,0,0,0.14),0px 5px 24px 4px rgba(0,0,0,0.12)",
+  "0px 7px 9px -4px rgba(0,0,0,0.2),0px 14px 21px 2px rgba(0,0,0,0.14),0px 5px 26px 4px rgba(0,0,0,0.12)",
+  "0px 8px 9px -5px rgba(0,0,0,0.2),0px 15px 22px 2px rgba(0,0,0,0.14),0px 6px 28px 5px rgba(0,0,0,0.12)",
+  "0px 8px 10px -5px rgba(0,0,0,0.2),0px 16px 24px 2px rgba(0,0,0,0.14),0px 6px 30px 5px rgba(0,0,0,0.12)",
+  "0px 8px 11px -5px rgba(0,0,0,0.2),0px 17px 26px 2px rgba(0,0,0,0.14),0px 6px 32px 5px rgba(0,0,0,0.12)",
+  "0px 9px 11px -5px rgba(0,0,0,0.2),0px 18px 28px 2px rgba(0,0,0,0.14),0px 7px 34px 6px rgba(0,0,0,0.12)",
+  "0px 9px 12px -6px rgba(0,0,0,0.2),0px 19px 29px 2px rgba(0,0,0,0.14),0px 7px 36px 6px rgba(0,0,0,0.12)",
+  "0px 10px 13px -6px rgba(0,0,0,0.2),0px 20px 31px 3px rgba(0,0,0,0.14),0px 8px 38px 7px rgba(0,0,0,0.12)",
+  "0px 10px 13px -6px rgba(0,0,0,0.2),0px 21px 33px 3px rgba(0,0,0,0.14),0px 8px 40px 7px rgba(0,0,0,0.12)",
+  "0px 10px 14px -6px rgba(0,0,0,0.2),0px 22px 35px 3px rgba(0,0,0,0.14),0px 8px 42px 7px rgba(0,0,0,0.12)",
+  "0px 11px 14px -7px rgba(0,0,0,0.2),0px 23px 36px 3px rgba(0,0,0,0.14),0px 9px 44px 8px rgba(0,0,0,0.12)",
+  "0px 11px 15px -7px rgba(0,0,0,0.2),0px 24px 38px 3px rgba(0,0,0,0.14),0px 9px 46px 8px rgba(0,0,0,0.12)",
+];
+
+const mediaQueries = Object.entries(breakpoints).reduce(
+  (prev, [key, val]) => ({
+    up: {
+      ...prev.up,
+      [key]: `@media screen and (min-width: ${val})`,
+    },
+    down: {
+      ...prev.down,
+      [key]: `@media screen and (max-width: ${val})`,
+    },
+  }),
+  {
+    up: {},
+    down: {},
+  }
+);
+
+export default {
+  colors,
+  fontSizes,
+  fontWeights,
+  space,
+  breakpoints,
+  shadows,
+  mediaQueries,
+};
